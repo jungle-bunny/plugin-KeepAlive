jungle-bunny/plugin-KeepAlive

commit fcc22524bf9ea067209c31cb323fe6de1227b0a4

start reinsertion next site after inactive state

src/keepalive/service/reinserter/Reinserter.java
@@ -166,7 +166,9 @@ public class Reinserter extends Thread {
 				parsedSegmentId = -1;
 				parsedBlockId = -1;
 				while (manifestURIs.size() > 0) {
-					if (!isActive()) return;
+					if (!isActive()) {
+						return;
+					}
 
 					uri = (FreenetURI) manifestURIs.keySet().toArray()[0];
 					log(uri.toString(), 0);
@@ -175,7 +177,9 @@ public class Reinserter extends Thread {
 
 				}
 
-				if (!isActive()) return;
+				if (!isActive()) {
+					return;
+				}
 
 				saveBlockUris();
 				plugin.setIntProp("blocks_" + siteId, blocks.size());
@@ -472,7 +476,10 @@ public class Reinserter extends Thread {
 					}
 
 					if (!isActive()) {
-						return;
+						// TODO: this is a bypass
+						plugin.log("Start reinsertion next site after inactive state", 0);
+						isActive(true);
+						break;
 					}
 
 					checkFinishedSegments();
@@ -1256,8 +1263,8 @@ public class Reinserter extends Thread {
 			return true;
 		}
 		if (lastActivityTime != Integer.MIN_VALUE) {
-			long nDelay = (System.currentTimeMillis() - lastActivityTime) / 60 / 1000;
-			return (nDelay < SingleJob.MAX_LIFETIME + 5);
+			long delay = (System.currentTimeMillis() - lastActivityTime) / 60_000; // delay in minutes
+			return (delay < SingleJob.MAX_LIFETIME + 1);
 		}
 		return false;
 	}

commit 0c908e1ce2aa81250c8a964c7de5d7ea79c1c3f2

fix stop button which was broken in a fcc2252

src/keepalive/service/reinserter/Reinserter.java
@@ -61,6 +61,7 @@ import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map.Entry;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.zip.ZipInputStream;
 
@@ -470,16 +471,21 @@ public class Reinserter extends Thread {
 
 			// wait for finishing all segments
 			if (doReinsertions) {
+				long waitingTime = System.currentTimeMillis();
 				while (plugin.getIntProp("segment_" + siteId) != maxSegmentId) {
 					synchronized (this) {
 						this.wait(1000);
 					}
 
-					if (!isActive()) {
-						// TODO: this is a bypass
-						plugin.log("Start reinsertion next site after inactive state", 0);
+					// TODO: this is a bypass
+					if (System.currentTimeMillis() - waitingTime > TimeUnit.MINUTES.toMillis(15)) {
+						plugin.log("Start reinsertion next site after stuck state", 0);
 						isActive(true);
 						break;
+					}
+
+					if (!isActive()) {
+						return;
 					}
 
 					checkFinishedSegments();
@@ -1264,7 +1270,7 @@ public class Reinserter extends Thread {
 		}
 		if (lastActivityTime != Integer.MIN_VALUE) {
 			long delay = (System.currentTimeMillis() - lastActivityTime) / 60_000; // delay in minutes
-			return (delay < SingleJob.MAX_LIFETIME + 1);
+			return (delay < SingleJob.MAX_LIFETIME + 5);
 		}
 		return false;
 	}