22cab2891338984d0ce74aee102bce8ad910e5d3
Oleh Shklyar <oleh.shklyar@redwerk.com>
2019-09-18 21:09:17 +0300

    new version

--- src/keepalive/Plugin.java
+++ src/keepalive/Plugin.java
@@ -36,7 +36,7 @@ import java.util.concurrent.*;
 
 public class Plugin extends PluginBase {
 
-    private static final String version = "0.3.3.8-RW";
+    private static final String version = "0.3.3.9-RW";
 
     private Thread reinserterRunner;
     private long propSavingTimestamp;
